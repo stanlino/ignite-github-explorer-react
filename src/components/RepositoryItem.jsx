bee026999d06372afb1a25adeea442d22b48355b
Stanley Filipe <stanley92filipe@gmail.com>
2022-03-22 13:50:51 -0300

    feat: style the RepositoryList component

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,14 +1,11 @@
 export function RepositoryItem(props) {
-
-  console.log(props.repository?.name)
-
   return (
     <li>
       <strong>{props.repository?.name ?? 'Default'}</strong>
       <p>{props.repository?.description ?? 'Default'}</p>
 
       <a href={props.repository?.link ?? '#'}>
-        {props.repository?.link ?? 'Default'}
+        Acessar repositório
       </a>
     </li>
   )
