c0dfb4304d0c69af52813c59a4c6c5af6096a1d9
Stanley Filipe <stanley92filipe@gmail.com>
2022-03-22 19:11:31 -0300

    build: add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map', 
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -15,7 +15,9 @@ module.exports = {
   resolve: {
     extensions: [
       '.jsx',
-      '.js'
+      '.js',
+      '.ts',
+      '.tsx'
     ]
   },
   devServer: {
@@ -31,7 +33,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
